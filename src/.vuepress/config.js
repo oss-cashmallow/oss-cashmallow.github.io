40b02ffcee0cbb247dbe18302a44416a155671dc
tom-mallow <mgg@cashmallow.com>
2022-12-25 21:54:30 +0900

    Build Basic Layout

---
+++ src/.vuepress/config.js
@@ -0,0 +1,126 @@
+import { defaultTheme, defineUserConfig } from 'vuepress'
+import { searchPlugin } from '@vuepress/plugin-search'
+import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+
+export default defineUserConfig({
+  sourceDir: "src",
+  dest: "docs/",
+  
+  lang: 'en-US',
+
+  title: 'team TIGER',
+  description: "cashmallow dev team's technology blog about wanting to contribute to the open source ecosystem",
+
+  head: [
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:title', content: 'tech-blog:cashmallow' }],
+    ['meta', { property: 'og:description', content: "cashmallow dev team's technology blog about wanting to contribute to the open source ecosystem" }],
+    ['meta', { property: 'og:image', content: 'https://user-images.githubusercontent.com/120996497/209457891-2c91f7fe-514d-4431-bfcb-7f6ee72ed8ee.png' }],
+    ['meta', { property: 'og:url', content: 'https://oss.cashmallow.com' }],
+  ],
+  
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    '/en/': {
+      lang: 'en-US',
+    //   title: 'tech-blog:cashmallow',
+    //   description: "cashmallow dev team's technology blog about wanting to contribute to the open source ecosystem",
+    },
+    '/': {
+      lang: 'ko-KR',
+    //   title: 'tech-blog:cashmallow',
+    //   description: "cashmallow dev team's technology blog about wanting to contribute to the open source ecosystem",
+    },
+  },
+
+
+  locales: {
+    '/zh/': {
+      lang: 'zh-CN',
+    },
+    '/en/': {
+      lang: 'en-US',
+    },
+    '/': {
+      lang: 'ko-KR',
+    },
+  },
+
+  theme: defaultTheme({
+    selectLanguageText: "lang",
+
+    locales: {
+      '/zh/': {
+        selectLanguageName: '简体中文',
+      },
+      '/en/': {
+        selectLanguageName: 'English',
+      },
+      '/': {
+        selectLanguageName: '한국어',
+      },
+    },
+
+    logo: "/images/tiger-stern.svg",
+    logoDark: "/images/tiger-cute.svg",
+
+    repo: "https://github.com/oss-cashmallow/oss-cashmallow.github.io/discussions",
+    repoLabel: "discussions",
+
+    contributors: false,
+
+    editLink: false,
+    // editLinkText: '✏️',
+
+    lastUpdated: true,
+    lastUpdatedText: "Last Updated",
+
+    colorMode: "auto",
+    colorModeSwitch: true,
+
+    navbar: [
+      { text: "recruit", link: "https://www.cashmallow.com" },
+      { text: "cashmallow", link: "https://www.cashmallow.com" },
+    ],
+
+    sidebar: {
+      "/": [
+        {
+          text: "share",
+          collapsible: true,
+          children: [
+            "/tech/share/",
+          ],
+        },
+        {
+          text: "team",
+          collapsible: true,
+          children: [
+            "/team/tiger/",
+          ],
+        },
+        {
+          text: "opensource",
+          collapsible: true,
+          children: [
+            "/tech/share/opensource/",
+          ],
+        },
+      ],
+    },
+  }),
+
+  plugins: [
+    searchPlugin({
+
+    }),
+    backToTopPlugin(),
+    googleAnalyticsPlugin({
+      id: "TODO",
+      debug: true,
+    }),
+  ],
+
+})
